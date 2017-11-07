18a8c66b7ffa8685a14a181ddf2bfd1d9aead0a4
Valentin BREDY <valentin.bredy@ynov.com>
2017-11-08 00:48:00 +0100

    First commit

--- CatCallAppProject/src/pages/home/home.ts
+++ CatCallAppProject/src/pages/home/home.ts
@@ -1,19 +1,77 @@
 import { Component } from '@angular/core';
-
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
 import { NavController } from 'ionic-angular';
+import { Geolocation, Geoposition } from 'ionic-native';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    Coordinates: any;
+    watch: any;
+    constructor(public navCtrl: NavController) {
 
-  constructor(public navCtrl: NavController) {
+    }
 
-  }
-  
-  onLink(url: string) {
-      window.open(url);
-  }
+    ionViewDidEnter() {
+
+        /*Initializing geolocation*/
+        let options = {
+            frequency: 3000,
+            enableHighAccuracy: true
+        };
+
+        this.watch = Geolocation.watchPosition(options)
+            .subscribe((position: Geoposition) => {
+                console.log(position);
+                this.Coordinates = position.coords;
+                this.executemap()
+            });
+    }
+
+
+    executemap() {
+
+        /*Initializing Map*/
+        mapboxgl.accessToken = 'pk.eyJ1IjoidmFsaWRlbyIsImEiOiJjajlxOGJzbWo1cnVlMnFxcXNlOG5odzdtIn0.C3pvn7CMluT7KUw6J7N8lQ';
+        var map = new mapboxgl.Map({
+            style: 'mapbox://styles/mapbox/streets-v10',
+            center: [this.Coordinates.longitude, this.Coordinates.latitude],
+            zoom: 16,
+            pitch: 40,
+            minZoom: 3, //restrict map zoom - buildings not visible beyond 13
+            maxZoom: 17,
+            container: 'map'
+        });
+        map.on('load', function () {
+            map.addLayer({
+                'id': '3d-buildings',
+                'source': 'composite',
+                'source-layer': 'building',
+                'filter': ['==', 'extrude', 'true'],
+                'type': 'fill-extrusion',
+                'minzoom': 15,
+                'paint': {
+                    'fill-extrusion-color': '#aaa',
+                    'fill-extrusion-height': {
+                        'type': 'identity',
+                        'property': 'height'
+                    },
+                    'fill-extrusion-base': {
+                        'type': 'identity',
+                        'property': 'min_height'
+                    },
+                    'fill-extrusion-opacity': .6
+                }
+            });
+        });
+        map.addControl(new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true
+        }));
+    }
 
 }
